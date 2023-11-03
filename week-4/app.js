f847d72633feb831df97dc3444c623cfe7eff3aa
ediiotero <e.otero81@gmail.com>
2023-11-02 20:07:06 -0400

    week 5 start

---
+++ week-4/app.js
@@ -0,0 +1,25 @@
+var name = "Eddie";
+var isHappy = true;
+var list = ['Tobi', 3, false];
+var obj = {key: "value", name: "Eddie"};
+var test = null;
+
+
+// console.log(name)
+// console.log(isHappy)
+// console.log(list)
+// console.log(obj)
+// console.log(test)
+
+var number = 5;
+var number2 = '5';
+var number3 = 2;
+
+// console.log('test 1', number == number2)
+// console.log('test 2', number === number2)
+
+console.log(number + number3)
+console.log(number - number3)
+console.log(number / number3)
+console.log(number * number3)
+console.log(number % number3)
