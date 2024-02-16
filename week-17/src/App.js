4f30439ac5ee01c9cf8b04148c5f06cd9c05c6d2
ediiotero <e.otero81@gmail.com>
2024-02-15 23:00:59 -0500

    week 17 done

--- week-17/src/App.js
+++ week-17/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import Age from './components/Age';
+import UserInfo from './components/UserInfo';
+import CompanyInfo from './components/CompanyInfo';
+import withUserId from './components/withUserId';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './store/index';
+
+const SomeUserInfo = withUserId(UserInfo)
+const SomeCompanyInfo = withUserId(CompanyInfo)
+
 
 function App() {
 
+  const account = useSelector(state => state.account)
+  const dispatch = useDispatch()
+
+  const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch)
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{account}</h1>
+      <div>
+        <button onClick={() => depositMoney(5)}>Deposit</button>
+        <button onClick={() => withdrawMoney(1)}>Withdraw</button>
+      </div>
+      <Age name={'Tobi'} age={3}/>
+      <SomeUserInfo userId={2}/>
+      <SomeCompanyInfo userId={2}/>
+      <SomeUserInfo userId={3}/>
+      <SomeCompanyInfo userId={3}/>
     </div>
   );
 }
